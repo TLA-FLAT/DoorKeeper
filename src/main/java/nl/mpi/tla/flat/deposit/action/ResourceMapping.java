99903407515af22b8ca4b175e8f6ea8abf815651
danrhe <daniel.vonrhein@gmail.com>
2017-03-01 11:36:40 +0100

    fix that SIP may also contain no resource folder

--- src/main/java/nl/mpi/tla/flat/deposit/action/ResourceMapping.java
+++ src/main/java/nl/mpi/tla/flat/deposit/action/ResourceMapping.java
@@ -24,11 +24,11 @@ public class ResourceMapping extends AbstractAction{
     @Override
     public boolean perform(Context context) throws DepositException {
 
-
         try {
             // Check for every CMDI resource (set of resources) if file is available and accessible
             Set<Resource> resources = context.getSIP().getResources();
-            for (Resource res:resources) {
+
+            for (Resource res : resources) {
                 File f = new File(Paths.get(res.getURI()).toString());
                 if (!f.exists()) {
                     throw new Exception("File with URI " + res.getURI() + " is not present in resource directory");
@@ -37,26 +37,34 @@ public class ResourceMapping extends AbstractAction{
                 }
             }
 
-            logger.info("All resources specified in record.cmdi are available and accesible");
-
-            // Check for every resource file in directory if entry in CMDI exists
-            // directory containing all resources
+            // Check if resource directory exists. In case no resources are specified that's okay, otherwise throw exception
             Path resourcesDir = Paths.get(getParameter("dir", "./resources"));
+            if (!resourcesDir.toFile().exists()) {
+                logger.debug("resources dir["+resourcesDir+"] doesn't exist!");
+                if (resources.size() == 0) {
+                   // Add debug info for empty resources
+                    logger.debug("No resources have been specified in record.cmdi");
+                } else {
+                    throw new Exception("resources directory does not exist");
+                }
+            } else {
+                    // Check in case of an existing resource directory if all files are also specified in record.cmdi
+                    FileWalker fileWalker = new FileWalker(resources);
+                    Files.walkFileTree(resourcesDir, fileWalker);
+                    if (fileWalker.foundResources.size() > fileWalker.matchingResources.size()) {
 
-            // Inventorize resources using a SimpleFileVisitor extension
-            FileWalker fileWalker = new FileWalker(resources);
-            Files.walkFileTree(resourcesDir, fileWalker);
-            if (fileWalker.foundResources.size() > fileWalker.matchingResources.size()){
+                        throw new Exception("Doorkeeper has found more files in resource directory [" + resourcesDir + "] than resources are specified in record.cmdi");
 
-                throw new Exception("Doorkeeper has found more files in resource directory [" + resourcesDir  + "] than resources are specified in record.cmdi");
+                        //TODO Maybe add feature to add missing resource specification to CMDI
+                    }
 
-                //TODO Maybe add feature to add missing resource specification to CMDI
             }
+
             logger.info("Record.cmdi resources neatly match up with available files in resource folder");
 
 
         } catch (Exception ex) {
-            throw new DepositException("Couldn't complete cmdi resource mapping",ex);
+            throw new DepositException("Couldn't complete cmdi resource mapping", ex);
         }
         return true;
 
