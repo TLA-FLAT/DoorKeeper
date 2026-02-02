e3fc8e75aaad5e9d3e0747fe335dd8fcf311b398
PaviSrinivasa <pavithra.srinivasa@mpi.nl>
2026-02-02 11:20:13 +0100

    FedoraTransaction
    initial commit.

--- src/main/java/nl/mpi/tla/flat/deposit/action/FedoraTransaction.java
+++ src/main/java/nl/mpi/tla/flat/deposit/action/FedoraTransaction.java
@@ -31,6 +31,7 @@ import net.sf.saxon.s9api.XdmItem;
 import net.sf.saxon.s9api.XdmNode;
 import nl.mpi.tla.flat.deposit.Context;
 import nl.mpi.tla.flat.deposit.DepositException;
+import static nl.mpi.tla.flat.deposit.action.FedoraAction.fedoraClient;
 import nl.mpi.tla.flat.deposit.sip.Collection;
 import nl.mpi.tla.flat.deposit.sip.Resource;
 import nl.mpi.tla.flat.deposit.sip.SIPInterface;
@@ -41,6 +42,7 @@ import org.apache.commons.io.filefilter.RegexFileFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import nl.mpi.tla.flat.deposit.util.Global;
+import org.apache.commons.configuration.XMLConfiguration;
 
 /**
  * @author menzowi
@@ -52,18 +54,94 @@ public class FedoraTransaction extends FedoraAction {
 
 	@Override
 	public boolean perform(Context context) throws DepositException {
-		try {
-			connect(context);
-                        // TODO look at the action name via this.getName() and determine what to do 
-                        // store the transaction uri via context.putInMemory
+            SIPInterface sip;
+            
+            try {
+                fedoraConfig = new XMLConfiguration(new File(getParameter("fedoraConfig")));               
+            } catch(Exception e) {
+                throw new DepositException("Connecting to Fedora Commons failed!",e);
+            }
+            try {
+                connect(context);
+                //sip = context.getSIP();
 
-		} catch (Exception e) {
-			throw new DepositException("The actual deposit in Fedora failed!", e);
-		}
-		return true;
+                // TODO look at the action name via this.getName() and determine what to do 
+                // store the transaction response uri via context.putInMemory
+
+                String actionName = this.getName(); 
+
+                if ("startTransaction".equals(actionName)) {
+                    logger.debug("Inside startTransaction.......");
+                    try {
+                        URI startUri = URI.create(fedoraConfig.getString("localServer")+"/fcr:tx");
+                        logger.debug("StartUri--"+startUri);
+                        TransactionalFcrepoClient transClient = fedoraClient.startTransactionClient(startUri);
+                        URI transLocation = transClient.getTransactionURI();
+                        context.putInMemory("transLocation",transLocation);
+                        logger.debug("TransLocaton: "+transLocation);
+                        logger.debug("TransLocaton context: "+context.getFromMemory("transLocation"));
+                        context.registerRollbackEvent(this,"TransLocation","putInMemory",transLocation.toString());
+                    }
+                    catch (Exception e) {
+                        throw new DepositException("Start Transaction Error: ", e);
+                    }
+                }
+                    
+                if ("commitTransaction".equals(actionName)) {
+                    
+                    //bring uri from the context memory and then append fcr:commit to it. 
+                    String contextUri = context.getFromMemory("transLocation").toString();
+                    logger.debug("Commit-- ContextUri--->"+contextUri);
+                    URI commitUri = URI.create(contextUri+"/fcr:commit");
+                    try (FcrepoResponse response = new PostBuilder(commitUri, fedoraClient).perform()) {
+                        logger.debug("Transaction commit status: {}", response.getStatusCode());
+                    }
+                    catch (Exception e) {
+                        throw new DepositException("Commit Transaction Error: ", e);
+                    }
+                }
+                    
+                if ("rollbackTransaction".equals(actionName)) {
+                    //use the uri from the memory and append fcr:rollback to it
+                    String contextUri = context.getFromMemory("transLocation").toString();
+                    logger.debug("rollback-- ContextUri--->"+contextUri);
+                    URI rollbackUri = URI.create(contextUri+"/fcr:rollback");
+                    try (FcrepoResponse response = new PostBuilder(rollbackUri, fedoraClient).perform()) {
+                        logger.debug("Transaction rollback status: {}", response.getStatusCode());
+                    }
+                    catch (Exception e) {
+                        throw new DepositException("Rollback Transaction Error: ", e);
+                    }
+                }
+            } catch (Exception e) {
+                    throw new DepositException("The actual deposit in Fedora failed!", e);
+            }
+            return true;
 	}
         
 	public void rollback(Context context, List<XdmItem> events) {
-	}
-
+            if (events.size() > 0) {
+                for (ListIterator<XdmItem> iter = events.listIterator(events.size()); iter.hasPrevious();) {
+                        XdmItem event = iter.previous();
+                        try {
+                                String tpe = Saxon.xpath2string(event, "@type");
+                                if(tpe=="TransLocation"){
+                                    String contextUri = context.getFromMemory("transLocation").toString();
+                                    logger.debug("rollback-- ContextUri--->"+contextUri);
+                                    URI rollbackUri = URI.create(contextUri+"/fcr:rollback");
+                                    try (FcrepoResponse response = new PostBuilder(rollbackUri, fedoraClient).perform()) {
+                                        logger.debug("Transaction rollback status: {}", response.getStatusCode());
+                                    }
+                                    catch (Exception e) {
+                                        throw new DepositException("Rollback Transaction Error: ", e);
+                                    }
+                                }
+                        }
+                        catch (Exception ex) {
+                                logger.error("rollback action[" + this.getName() + "] event[" + event + "] failed!", ex);
+                        }
+                }
+            }
+                       
+        }
 }
